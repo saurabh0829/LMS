6aa38a81055243aa7e0509db612b2508ad7b0a2b
saurabh0829 <saurabhshrma0829@gmail.com>
2026-04-06 22:01:02 +0530

    mongoose-connection

--- server/config/mongodb.js
+++ server/config/mongodb.js
@@ -3,17 +3,9 @@ import mongoose from "mongoose";
 // Connect to the mongodb database
 
 const connectDB = async ()=>{
-    try {
-        mongoose.connection.on('connected', ()=> console.log('Database Connected'))
-        
-        await mongoose.connect(`${process.env.MONGODB_URI}/lms`, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        })
-    } catch (error) {
-        console.error('Database connection error:', error)
-        throw error
-    }
+    mongoose.connection.on('connected', ()=> console.log('Database Connected'))
+
+    await mongoose.connect(`${process.env.MONGODB_URI}/lms`)
 }
 
 export default connectDB
